3161257c978c44a7f7669237526df28c7cb27be0
cwmonkey <wackoburns@hotmail.com>
2025-06-08 11:34:46 -0700

    Fixing syntax

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 export JEKYLL_ENV="prod"
-del /f /q /s _site/*.* > NUL
+del /f /q /s _site > NUL
 rmdir /q /s _site
 bundle exec jekyll serve --watch --config _config.yml,_config.local.yml
 REM bundle exec jekyll serve --host=0.0.0.0 --port 4001 --watch --config _config.local.yml,_config.yml --incremental
